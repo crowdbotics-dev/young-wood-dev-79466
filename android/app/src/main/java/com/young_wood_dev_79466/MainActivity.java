2198429e04ded8c32655cc643380fdc4dad9da5d
Crowdbotics <engineering@crowdbotics.com>
2023-04-27 23:17:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/young_wood_dev_79466/MainActivity.java
@@ -0,0 +1,15 @@
+package com.young_wood_dev_79466;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "young_wood_dev_79466";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "young-wood-dev-79466",
+  url: "https://young-wood-dev-79466.botics.co",
+  api: "https://young-wood-dev-79466.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
